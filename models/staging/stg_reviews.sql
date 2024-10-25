2c89cac713bde6078c4122ce7c8057349e642be9
nizam-git-hub <nizamdhin.s@gmail.com>
2024-10-25 16:50:01 +0530

    Delete models directory

--- models/staging/stg_reviews.sql
+++
@@ -1,7 +0,0 @@
-select
-   listing_id
-    , date::date as date
-    , reviewer_name
-    , initcap(comments) as comments
-    , upper(sentiment) as sentiment
-from {{ source('raw', 'raw_reviews') }}
