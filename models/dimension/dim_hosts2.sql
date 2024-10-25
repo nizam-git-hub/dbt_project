b5dd58a2403c563c0d603c7596fbe23933ce4f92
Nijamudhin S <nizamexplains@gmail.com>
2024-10-25 14:58:52 +0000

    q

--- models/dimension/dim_hosts2.sql
+++
@@ -1,8 +0,0 @@
-select 
-    {{dbt_utils.generate_surrogate_key(['host_id','name'])}} as sur,
-    host_id,
-    name,
-    is_superhost,
-    created_at,
-    updated_at
-from {{ ref('stg_hosts') }} 
